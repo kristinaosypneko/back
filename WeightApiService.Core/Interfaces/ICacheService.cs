a0c3ead5d841de8f1a786c344aa2e216f15e46b1
kristinaosypneko <kristinaosypneko@gmail.com>
2025-05-17 06:46:17 +0300

    Delete WeightApiService.Core directory

--- WeightApiService.Core/Interfaces/ICacheService.cs
+++
@@ -1,8 +0,0 @@
-namespace WeightApiService.Core.Interfaces;
-
-public interface ICacheService
-{
-    Task<T?> GetAsync<T>(string key);
-    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
-    Task RemoveAsync(string key);
-}
